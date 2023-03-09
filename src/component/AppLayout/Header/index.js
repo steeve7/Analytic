3908d3e90dfede4a989e36f9d240ea6e38c6b51b
ofoegbu stephen <stephenchidindu12@gmail.com>
2023-03-09 11:18:07 +0100

    initial commit

--- src/component/AppLayout/Header/index.js
+++ src/component/AppLayout/Header/index.js
@@ -1,86 +1,155 @@
-import React, { useState } from 'react'
-import Logo from './assest/Hernalytics Full Logo Yellow@3x 1(1).png'
-import NavLinks from './navlinks'
-import Signup from './signup'
-import {FiMenu} from 'react-icons/fi'
-import {MdClose} from 'react-icons/md'
+import React, { useState,useEffect} from "react";
+import Logo from "./assest/Hernalytics Full Logo Yellow@3x 1(1).png";
+import NavLinks from "./navlinks";
+import Signup from "./signup";
+import { FiMenu } from "react-icons/fi";
+import { MdClose } from "react-icons/md";
 
 const Index = () => {
-    const [open, setOpen] = useState(false)
+  const [open, setOpen] = useState(false);
+  const [candidates, setCandidate] = useState([]);
 
+  const getData = () => {
+    fetch("http://localhost:3001/candidate",{
+        header:{
+            "Content-Type": 'application/json',
+            "Accept": 'application/json'
+        }
+    }).then((res) => res.json())
+    .then((data) => {setCandidate(data)})
+}
+useEffect(()=> {
+    getData();
+},[])
 
   return (
     <>
-     <nav className="bg-[#1B1F30]">
-       <div className='flex items-center font-medium justify-around'>
-            <div className='z-50 p-5 md:w-auto w-full flex justify-between'>
-                <img src={Logo} alt="logo" className='md:cursor-pointer h-9' />
-                <div className='text-3xl md:hidden text-white' onClick={() =>setOpen(!open)}>
-                   {open ? <MdClose/> :<FiMenu/> }
-                </div>
+      <nav className="bg-[#1B1F30]">
+        <div className="flex items-center font-medium justify-around">
+          <div className="z-50 p-5 md:w-auto w-full flex justify-between">
+            <img src={Logo} alt="logo" className="md:cursor-pointer h-9" />
+            <div
+              className="text-3xl md:hidden text-white"
+              onClick={() => setOpen(!open)}
+            >
+              {open ? <MdClose /> : <FiMenu />}
             </div>
-            <ul className='md:flex hidden uppercase items-center gap-8 font-[Poppins] text-white'>
-                <li>
-                    <a href="/#" className="py-7 px-3 inline-block">
-                        About us
-                    </a>
-                </li>
-                <NavLinks/>
-            </ul>
-            <div className='md:block hidden'>
-                <Signup/>
+          </div>
+          <ul className="md:flex hidden uppercase items-center gap-8 font-[Poppins] text-white">
+            <li>
+              <a href="/#" className="py-7 px-3 inline-block">
+                About us
+              </a>
+            </li>
+            <NavLinks />
+          </ul>
+          <div className="md:block hidden">
+            <Signup />
+          </div>
+          {/*mobile nav*/}
+          <ul
+            className={`
+            md:hidden bg-white absolute w-full bottom-0 py-24 pl-4 duration-500 ${
+              open ? "left-0" : "left-[-100%]"
+            }`}
+          >
+            <li>
+              <a href="/#" className="py-7 px-3 inline-block">
+                About us
+              </a>
+            </li>
+            <NavLinks />
+            <div className="py-5">
+              <Signup />
             </div>
-            {/*mobile nav*/}
-            <ul className={`
-            md:hidden bg-white absolute w-full bottom-0 py-24 pl-4 duration-500 ${open ? "left-0" : "left-[-100%]"}`}>
-                <li>
-                    <a href='/#' className="py-7 px-3 inline-block">
-                    About us
-                    </a>
-                </li>
-                <NavLinks/>
-                <div className='py-5'>
-                    <Signup/> 
-                </div>
-            </ul>
-       </div> 
-    </nav>
-    <div className='uppercase text-white flex items-center space-x-4 m-10 ml-16'>
-        <h1 className='text-3xl font-bold'>Post election data</h1>
-        <h5 className='bg-[#1C2031] border border-gray-700 font-bold'>Presidential / 2023</h5>
-    </div>
+          </ul>
+        </div>
+      </nav>
+      <div className="uppercase text-white flex items-center space-x-4 m-10 ml-16">
+        <h1 className="text-3xl font-bold">Post election data</h1>
+        <h5 className="bg-[#1C2031] border border-gray-700 font-bold">
+          Presidential / 2023
+        </h5>
+      </div>
 
-    <div className='m-10 ml-16 flex items-center justify-between'>
+      <div className="m-10 ml-16 flex items-center justify-between">
         <h1 className=" text-white text-2xl font-bold">Presidential Race</h1>
         <div>
-        <select name="Election Date" id="" className='m-10 w-[20%] py-2 px-3 bg-[#1C2031] text-[#C6C7CB] border border-gray-700'>
-            <option value="Pre-Election" className='border-none'>Pre-Election</option>
-            <option value="Election Day Live Update" className='border-none'>Election Day Live Update</option>
-            <option value="Post-Election Analysis" className='border-none'>Post-Election Analysis</option>
-        </select>
-        <select name="Election Date" id="" className='m-10 w-[20%] py-2 px-3 bg-[#1C2031] text-[#C6C7CB] border border-gray-700'>
-            <option value="Presidential" className='border-none'>Presidential</option>
-            <option value="Gubernatorial" className='border-none'>Gubernatorial</option>
-            <option value="Senate" className='border-none'>Senate</option>
-            <option value="House Of Representative" className='border-none'>House Of Representative</option>
-        </select>
-        <select name="Election Date" id="" className='m-10 w-[20%] py-2 px-3 bg-[#1C2031] text-[#C6C7CB] border border-gray-700'>
-            <option value="2023" className='border-neutral-600'>2023</option>
-            <option value="2019" className='border-none'>2019</option>
-            <option value="2015" className='border-none'>2015</option>
-            <option value="2011" className='border-none'>2011</option>
-            <option value="2007" className='border-none'>2007</option>
-            <option value="2003" className='border-none'>2003</option>
-            <option value="1999" className='border-none'>1999</option>
-            <option value="1991" className='border-none'>1991</option>
-            
-        </select>
-    </div>
-    </div>
-    <hr className="md:w-[90%] ml-16 w-full md:mt-[-20px] absolute border-gray-700 flex justify-content" />
+          <select
+            name="Election Date"
+            id=""
+            className="m-10 w-[20%] py-2 px-3 bg-[#1C2031] text-[#C6C7CB] border border-gray-700"
+          >
+            <option value="Pre-Election" className="border-none">
+              Pre-Election
+            </option>
+            <option value="Election Day Live Update" className="border-none">
+              Election Day Live Update
+            </option>
+            <option value="Post-Election Analysis" className="border-none">
+              Post-Election Analysis
+            </option>
+          </select>
+          <select
+            name="Election Date"
+            id=""
+            className="m-10 w-[20%] py-2 px-3 bg-[#1C2031] text-[#C6C7CB] border border-gray-700"
+          >
+            <option value="Presidential" className="border-none">
+              Presidential
+            </option>
+            <option value="Senate" className="border-none">
+              Senate
+            </option>
+            <option value="House Of Representative" className="border-none">
+              House Of Representative
+            </option>
+          </select>
+          <select
+            name="Election Date"
+            id=""
+            className="m-10 w-[20%] py-2 px-3 bg-[#1C2031] text-[#C6C7CB] border border-gray-700"
+          >
+            <option value="2023" className="border-neutral-600">
+              2023
+            </option>
+            <option value="2019" className="border-none">
+              2019
+            </option>
+            <option value="2015" className="border-none">
+              2015
+            </option>
+            <option value="2011" className="border-none">
+              2011
+            </option>
+            <option value="2007" className="border-none">
+              2007
+            </option>
+            <option value="2003" className="border-none">
+              2003
+            </option>
+            <option value="1999" className="border-none">
+              1999
+            </option>
+            <option value="1991" className="border-none">
+              1991
+            </option>
+          </select>
+        </div>
+      </div>
+      <div className="flex flex-col md:flex-row w-full justify-between space-x-7 text-white m-auto px-10">
+           {candidates.map((candidate, index) => (
+            <div key={index}  className="shadow-lg rounded-lg w-full
+             justify-center text-center py-7">
+             <h1 className="uppercase">candidate name: {candidate.full_name}</h1>
+                <p className="uppercase">Party name: {candidate.political_party_name}</p>
+                <span className="uppercase">Total Vote: {candidate.candidates_vote}</span>
+            </div>
+           ))} 
+      </div>
+      <hr className="md:w-[90%] ml-16 w-full md:mt-[-20px] absolute border-gray-700 flex justify-content" />
     </>
-   
-  )
-}
+  );
+};
 
-export default Index
+export default Index;
--- src/component/section/Analysis/index.js
+++ src/component/section/Analysis/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import Circle from './assest/Ellipse 2.png'
 import Circle2 from './assest/Ellipse 1.png'
 import GeoImgae from './assest/ng 1.png'
@@ -6,66 +6,183 @@ import Male from './assest/Vector(9).png'
 import Female from './assest/Vector(10).png'
 
 
-const index = () => {
+const Index = () => {
+    const [abias, setAbia] = useState([]);
+    const [adamawas, setAdamawa] = useState([]);
+    const [anambras, setAnambra] = useState([]);
+    const [crosss, setCross] = useState([]);
+    const [lagoss, setLagos] = useState([]);
+    const [riverss, setRivers] = useState([]);
+
+    const getData = () => {
+        fetch("http://localhost:30001/Abia",{
+            header:{
+                "Content-Type": 'application/json',
+                "Accept": 'application/json'
+            }
+        }).then((res) => res.json())
+        .then((data) => {setAbia(data)})
+    }
+    const getState = () => {
+        fetch("http://localhost:30001/Adamawa",{
+            header:{
+                "Content-Type": 'application/json',
+                "Accept": 'application/json'
+            }
+        }).then((res) => res.json())
+        .then((data) => {setAdamawa(data)})
+    }
+    const getAnambra = () => {
+        fetch("http://localhost:30001/Anambra",{
+            header:{
+                "Content-Type": 'application/json',
+                "Accept": 'application/json'
+            }
+        }).then((res) => res.json())
+        .then((data) => {setAnambra(data)})
+    }
+    const getCross = () => {
+        fetch("http://localhost:30001/CrossRiver",{
+            header:{
+                "Content-Type": 'application/json',
+                "Accept": 'application/json'
+            }
+        }).then((res) => res.json())
+        .then((data) => {setCross(data)})
+    }
+    const getLagos = () => {
+        fetch("http://localhost:30001/Lagos",{
+            header:{
+                "Content-Type": 'application/json',
+                "Accept": 'application/json'
+            }
+        }).then((res) => res.json())
+        .then((data) => {setLagos(data)})
+    }
+    const getRivers = () => {
+        fetch("http://localhost:30001/Rivers",{
+            header:{
+                "Content-Type": 'application/json',
+                "Accept": 'application/json'
+            }
+        }).then((res) => res.json())
+        .then((data) => {setRivers(data)})
+    }
+
+    useEffect(()=> {
+        getData();
+        getState();
+        getAnambra();
+        getCross();
+        getLagos();
+        getRivers();
+    },[])
+
   return (
     <section className='flex justify-content m-auto items-center border-gray-700 mt-20 border w-[90%] shadow-lg py-20'>
-        <div className='border border-gray-700 py-[90px] m-7 rounded-lg shadow-md px-10'>
+        <div className='border border-gray-700 py-[205px] m-7 rounded-lg shadow-md px-10'>
         <img src={Circle2} alt="design" className='w-[250px]' />
         <img src={Circle} alt="design" className='w-[200px] ml-20 absolute mt-[-200px] left-[57px]' />
-        <div className='uppercase text-white text-center absolute mt-[-120px] ml-12'>
+        <div className='uppercase text-white text-center absolute mt-[-120px] ml-[67px]'>
         <h1 className='text-[10px]'>Total Votes</h1>
-        <p className='font-bold'>168,912,222</p>
+        <p className='font-bold'>112,881</p>
         </div>
         <div className='uppercase text-white text-center absolute mt-[-250px] ml-36'>
-        <h1 className='text-[10px]'>Rejected Votes</h1>
-        <p className='font-bold'>8,400,000</p>
+        <h1 className='text-[10px]'>PDP votes</h1>
+        <p className='font-bold'>6,216</p>
         </div>
         <div className='uppercase text-white text-center absolute '>
-        <h1 className='text-[10px]'>Accepted Votes</h1>
-        <p className='font-bold'>160,512,222</p>
+        <h1 className='text-[10px]'>LP Votes</h1>
+        <p className='font-bold'>104,453</p>
+        </div>
+        <div className='uppercase text-white text-center absolute top-[1508px] left-40 '>
+        <h1 className='text-[10px]'>APC Votes</h1>
+        <p className='font-bold'>2,212</p>
         </div>
        
         </div>
         <div className='border border-gray-700 py-10 m-7 rounded-lg shadow-md px-10 w-[623px]'>
-            <h1 className='text-white mb-7 text-center uppercase font-[Poppins]'>Candidates by geopolitical zones</h1>
+            <h1 className='text-white text-center uppercase font-[Poppins]'>Candidates by States</h1>
             <div className='flex items-center text-white'>
-            <img src={GeoImgae} alt="geoimage" />
-            <div className='flex flex-col w-full text-[10px] ml-5 mt-20 space-y-4'>
+            <img src={GeoImgae} alt="geoimage" className='hidden' />
+            <div className='flex flex-col w-full text-[10px] ml-5 mt-10 space-y-4'>
             <div className='flex items-center space-x-2'>
             <div className='bg-[#449352] w-[20px] h-[10px] relative'/>
-                <p>North East</p>
-                <p>3,000</p>
-            </div> 
+            <h1 className="font-bold text-[12px]">ABIA STATE</h1>
+            </div>
             <div className='flex items-center space-x-2'>
-            <div className='bg-[#6D769D] w-[20px] h-[10px] relative'/>
-                <p>North west</p>
-                <p>3,000</p>
-            </div> 
-            <div className='flex items-center space-x-2'>
-            <div className='bg-[#A163BE] w-[20px] h-[10px] relative'/>
-                <p>North Central</p>
-                <p>3,000</p>
+                {abias.map((abia, index) => (
+                    <div className='space-x-2 text-center' key={index}>                   
+                    <p>{abia.political_party_name}</p>
+                    <p>{abia.candidate_votes}</p>
+                    </div>
+                ))}
             </div> 
             <div className='flex items-center space-x-2'>
             <div className='bg-[#E30325] w-[20px] h-[10px] relative'/>
-                <p>South West</p>
-                <p>3,000</p>
+            <h1 className="font-bold text-[12px]">ADAMAWA STATE</h1>
+            </div>
+            <div className='flex items-center space-x-2'>
+                {adamawas.map((adamawa, index) => (
+                    <div className='space-x-2 text-center' key={index}>
+                    <p>{adamawa.political_party_name}</p>
+                    <p>{adamawa.candidate_votes}</p>
+                    </div>
+                ))}
+            </div>
+            <div className='flex items-center space-x-2'>
+            <div className='bg-[#449352] w-[20px] h-[10px] relative'/>
+            <h1 className="font-bold text-[12px] uppercase">anambra state</h1>
+            </div>
+            <div className='flex items-center space-x-2'>
+                {anambras.map((anambra, index) => (
+                    <div className='space-x-2 text-center' key={index}>
+                    <p>{anambra.political_party_name}</p>
+                    <p>{anambra.candidate_votes}</p>
+                    </div>
+                ))}
             </div> 
             <div className='flex items-center space-x-2'>
-            <div className='bg-[#2249D1] w-[20px] h-[10px] relative'/>
-                <p>South South</p>
-                <p>3,000</p>
+            <div className='bg-[#449352] w-[20px] h-[10px] relative'/>
+            <h1 className="font-bold text-[12px] uppercase">Cross River state</h1>
+            </div>
+            <div className='flex items-center space-x-2'>
+                {crosss.map((cross, index) => (
+                    <div className='space-x-2 text-center' key={index}>
+                    <p>{cross.political_party_name}</p>
+                    <p>{cross.candidate_votes}</p>
+                    </div>
+                ))}
             </div> 
             <div className='flex items-center space-x-2'>
-            <div className='bg-[#018796] w-[20px] h-[10px] relative'/>
-                <p>South East</p>
-                <p>3,000</p>
-            </div> 
+            <div className='bg-[#449352] w-[20px] h-[10px] relative'/>
+            <h1 className="font-bold text-[12px] uppercase">Lagos state</h1>
+            </div>
+            <div className='flex items-center space-x-2'>
+                {lagoss.map((lagos, index) => (
+                    <div className='space-x-2 text-center'>
+                    <p>{lagos.political_party_name}</p>
+                    <p>{lagos.candidate_votes}</p>
+                    </div>
+                ))}
+            </div>
+            <div className='flex items-center space-x-2'>
+            <div className='bg-[#449352] w-[20px] h-[10px] relative'/>
+            <h1 className="font-bold text-[12px] uppercase">Rivers state</h1>
+            </div>  
+            <div className='flex items-center space-x-2'>
+                {riverss.map((river, index) => (
+                    <div className='space-x-2 text-center'key={index}>
+                    <p>{river.political_party_name}</p>
+                    <p>{river.candidate_votes}</p>
+                    </div>
+                ))}
+            </div>  
             </div>
             </div>
         </div>
-        <div className='border border-gray-700 m-4 py-32 rounded-lg shadow-md w-[362px]'>
-        <h1 className='text-white mb-7 text-left ml-5 uppercase font-[Poppins] w-full mt-[-100px]'>Total elected member</h1>
+        <div className='border border-gray-700 m-4 py-72 rounded-lg shadow-md w-[362px]'>
+        <h1 className='text-white mb-7 text-left ml-5 uppercase font-[Poppins] w-full mt-[-200px]'>Total elected member</h1>
         <p className='font-bold text-white ml-5 mt-[-30px]'>10,000</p>
         <div className='flex item-center mt-10 ml-5 text-white space-x-2'>
             <h1>Male</h1>
@@ -82,4 +199,4 @@ const index = () => {
   )
 }
 
-export default index
+export default Index
